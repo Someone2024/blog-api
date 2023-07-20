88607d8bd55f68d0f2147fe7838ef0a4a623f2af
santiago <bassilsantiago@gmail.com>
2023-07-20 13:06:08 -0500

    Feat: Add login post request handler

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const PostsController = require("../Controllers/PostsController")
 const CommentsController = require("../Controllers/CommentsController")
+const authController = require("../Controllers/JWTController")
+const checkAuth = require("../Controllers/checkAuth")
+
+router.post("/login", authController.login)
 
 router.get("/posts/", PostsController.GETAllPosts)
 router.get("/posts/:postid", PostsController.GETPost)
@@ -9,13 +13,12 @@ router.get("/posts/:postid", PostsController.GETPost)
 router.get("/posts/:postid/comments/", CommentsController.GETAllComments)
 router.get("/posts/:postid/comments/:commentid", CommentsController.GETComment)
 
-router.post("/posts/create-post", PostsController.createPost)
-router.post("/posts/:postid/comments/create-comment", CommentsController.createComment)
+router.post("/posts/create-post", checkAuth.checkAuth, PostsController.createPost)
+router.post("/posts/:postid/comments/create-comment", checkAuth.checkAuth, CommentsController.createComment)
 
-router.put("/posts/update-post/:postid", PostsController.updatePost)
-router.put("/posts/:postid/comments/:commentid")
+router.put("/posts/update-post/:postid", checkAuth.checkAuth, PostsController.updatePost)
 
-router.delete("/posts/delete-post/:postid", PostsController.deletePost)
-router.delete("/posts/:postid/comments/delete-comment/:commentid", CommentsController.deleteComment)
+router.delete("/posts/delete-post/:postid", checkAuth.checkAuth, PostsController.deletePost)
+router.delete("/posts/:postid/comments/delete-comment/:commentid", checkAuth.checkAuth, CommentsController.deleteComment)
 
 module.exports = router;

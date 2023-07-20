ee1d95ea58e3d7deebcfeacc2b2baedb31fe9cd9
santiago <bassilsantiago@gmail.com>
2023-07-20 11:31:00 -0500

    Feat: added routes to perform crud operations

---
+++ routes/api.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const PostsController = require("../Controllers/PostsController")
+const CommentsController = require("../Controllers/CommentsController")
+
+router.get("/posts/", PostsController.GETAllPosts)
+router.get("/posts/:postid", PostsController.GETPost)
+
+router.get("/posts/:postid/comments/", CommentsController.GETAllComments)
+router.get("/posts/:postid/comments/:commentid", CommentsController.GETComment)
+
+router.post("/posts/create-post", PostsController.createPost)
+router.post("/posts/:postid/comments/create-comment", CommentsController.createComment)
+
+router.put("/posts/:postid")
+router.put("/posts/:postid/comments/:commentid")
+
+router.delete("/posts/:postid")
+router.delete("/posts/:postid/comments/:commentid")
+
+module.exports = router;

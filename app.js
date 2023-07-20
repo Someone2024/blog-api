62fde3dac9c3b35fc6d87aa650ce483f253809c4
santiago <bassilsantiago@gmail.com>
2023-07-20 11:33:23 -0500

    Update: Delete unuser font-end code

--- app.js
+++ app.js
@@ -18,9 +18,6 @@ async function main() {
     console.error(e)
   }
 }
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -42,8 +39,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).json({error: 'error'});
 });
 
 module.exports = app;
